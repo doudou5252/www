115fa380358dbee7fff03a90e0462ef083ae0efc
doudou1212 <1358818004@qq.com>
2019-02-25 10:53:41 +0800

    999

--- fruit/apps/user/urls.py
+++ fruit/apps/user/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from user import views
-from user.views import RegisterView, ActiveView
+from user.views import RegisterView, ActiveView, LoginView
 
 urlpatterns = [
     # url(r'^register$', views.register, name='register'),  # 注册
@@ -8,4 +8,6 @@ urlpatterns = [
 
     url(r'^register$', RegisterView.as_view(), name='register'),  # 注册
     url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 用户激活  (?P<token>.*) 传过来的参数
+
+    url(r'^login$', LoginView.as_view(), name='login'),  # 登录
 ]
