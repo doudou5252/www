f3a0833e3797f63f88f1f3afd3202849d4188624
doudou1212 <1358818004@qq.com>
2019-02-25 13:58:27 +0800

    999

--- fruit/apps/user/urls.py
+++ fruit/apps/user/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from user import views
 from user.views import RegisterView, ActiveView, LoginView
 
+app_name = 'user'  # 解决名称空间的问题
 urlpatterns = [
     # url(r'^register$', views.register, name='register'),  # 注册
     # url(r'^register_handle$', views.register_handle, name='register_handle'),  # 注册处理
