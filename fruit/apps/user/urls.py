5477266043edddb87dbedac43582429022c7f9cf
doudou1212 <1358818004@qq.com>
2019-02-27 13:29:14 +0800

    888

--- fruit/apps/user/urls.py
+++ fruit/apps/user/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # url(r'^active/(?P<token>.*)$', ActiveView.as_view(), name='active'),  # 用户激活  (?P<token>.*) 传过来的参数
     url(r'^active/(.*)$', ActiveView.as_view(), name='active'),
     url(r'^login$', LoginView.as_view(), name='login'),  # 登录
+    url(r'^logout$', LogoutView.as_view(), name='logout'),  # 注销登录
     url(r'^$', UserInfoView.as_view(), name='user'),  # 用户中心-信息页
     url(r'^order$', UserOrderView.as_view(), name='order'),  # 用户中心-订单页
     url(r'^address$', AddressView.as_view(), name='address'),  # 用户中心-地址页
